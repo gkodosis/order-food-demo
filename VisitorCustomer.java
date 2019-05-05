c74b26a2546a65637ef98bcfbd29c7fb6be0370b
Kaselouris Thanos <kaselouristhanos@yahoo.com>
2019-05-05 22:34:06 +0300

    Update VisitorCustomer.java

--- VisitorCustomer.java
+++ VisitorCustomer.java
@@ -3,18 +3,36 @@
  * meaning they decide to explore our platform
  * without using any credentials
  * @author gkodosis
- * 
+ * @author kasthanos
  */
 public class VisitorCustomer extends Customer {
+	/**
+	 * Full constructor
+	 *
+	 * @param fullname
+	 *        VisitorCustomer's name.
+	 * @param phoneNumber
+	 *        VisitorCustomer's phone.
+	 * @param address
+	 *        VisitorCustomer's address.
 
+	*/
 	public VisitorCustomer(String fullname, String phoneNumber, String address) {
 		super(fullname, phoneNumber, address);
 	}
 	
+	/**
+	* Default constructor
+	*
+	*/
+
+	public VisitorCustomer(){
+
+	}
 	@Override
 	public void getMenu() {
 		System.out.println("-- Browse Available Shops: Press 1");
 		System.out.println("-- Preview Shop's Products: Press 1");
 	}
 	
-}
+}//End of class
