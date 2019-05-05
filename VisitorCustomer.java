ff40ae00f00c3019715dd9827374ad35a26b86a3
Kaselouris Thanos <kaselouristhanos@yahoo.com>
2019-05-06 00:16:21 +0300

    Update VisitorCustomer.java
    Προσθηκη μενου (1)

--- VisitorCustomer.java
+++ VisitorCustomer.java
@@ -1,3 +1,7 @@
+package Ergasia2019;
+
+import java.util.Scanner;
+
 /**
  * This class describes the visitor customers of,
  * meaning they decide to explore our platform
@@ -15,12 +19,11 @@ public class VisitorCustomer extends Customer {
 	 *        VisitorCustomer's phone.
 	 * @param address
 	 *        VisitorCustomer's address.
-
 	*/
 	public VisitorCustomer(String fullname, String phoneNumber, String address) {
 		super(fullname, phoneNumber, address);
 	}
-	
+
 	/**
 	* Default constructor
 	*
@@ -32,7 +35,31 @@ public class VisitorCustomer extends Customer {
 	@Override
 	public void getMenu() {
 		System.out.println("-- Browse Available Shops: Press 1");
-		System.out.println("-- Preview Shop's Products: Press 1");
+		System.out.println("-- Preview Shop's Products: Press 2");
+		System.out.println();
+		System.out.println("--> Choice: ");
+		// Gather user's choice
+		Scanner input = new Scanner(System.in);
+		int choice = input.nextInt();
+		Shop shop = new Shop();
+		// Flow based on user's choice
+		while(choice != 0) {
+			if(choice == 1) {
+				System.out.println("-- Please provide us with the part of brand: ");
+				System.out.println();
+				System.out.println("--> Choice: ");
+				String line = input.next();
+				if (line.length() > 0){
+					shop.filterAndPrintShops(line);
+				}else {
+					shop.printAllShops();
+				}
+				getMenu();
+
+			} else if(choice == 2) {
+
+				getMenu();
+			}
+		}
 	}
-	
 }//End of class
