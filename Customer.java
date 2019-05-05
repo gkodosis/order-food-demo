30cb113c010bb5f9d42f19f8d807fb61a824d6b2
Kaselouris Thanos <kaselouristhanos@yahoo.com>
2019-05-05 22:04:46 +0300

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -2,22 +2,44 @@ import java.util.ArrayList;
 
 /**
  * This abstract class aims at designing the base 
- * characteristics of each customer
+ * characteristics of each Customer
+ *
  * @author gkodosis
+ * @author kasthanos
  *
  */
 public abstract class Customer {
 	
-	private String fullname;
-	private String phoneNumber;
-	private String address;
-	ArrayList<Customer> customer = new ArrayList<Customer>();
+	private String fullname; //customer's fullname
+	private String phoneNumber; //customer's phoneNumber
+	private String address; //customer's address
+	ArrayList<Customer> customer = new ArrayList<Customer>(); //arraylist to store the Customers
 	
+	/**
+	* Full constructor
+	*
+	* @param fullname
+	*        Customer's fullname.
+	* @param phone
+	*        Customer's phoneNumber.
+	* @param address
+	*        Customer's address.
+	*/
+
 	public Customer(String fullname, String phoneNumber, String address) {
 		this.fullname = fullname;
 		this.phoneNumber = phoneNumber;
 		this.address = address;
-		customer.add(this);
+		customer.add(this); //add object to the arraylist
+	}
+	
+	/**
+	* Default constructor
+	*
+	*/
+
+	public Customer(){
+
 	}
 
 	public void setCustomerName(String fullname) {
@@ -44,13 +66,19 @@ public abstract class Customer {
 		return address;
 	}
 	
-	// get menu based on user's type
+	// get menu based on customer's type
 	public abstract void getMenu();
 	
 	@Override
 	public String toString() {
-		return "--Fullname: " + getCustomerName() + " --Phone Number: " + getPhoneNumber()
-			+ " --Address: " + getAddress();
+		String pdata = "----------------------" + "\n"
+				+ "Customer Personal Data:" + "\n"
+				+ "----------------------" + "\n"
+				+ "Fullname: " + getCustomerName() + "\n"
+				+ "Phone Number: " + getPhoneNumber() + "\n"
+				+ "Address: " + getAddress();
+
+		return pdata;
 	}
 		
-}
+}// End of class
