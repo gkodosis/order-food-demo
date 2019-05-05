4e47ed1fff50f029bfd737769ca3dc88c435ffb8
Kaselouris Thanos <kaselouristhanos@yahoo.com>
2019-05-05 22:39:11 +0300

    Update Product.java

--- Product.java
+++ Product.java
@@ -2,19 +2,37 @@
  * This class includes all the products distributed
  * from our platform and their specifications 
  * @author gkodosis
- * 
+ * @author kasthanos
  */
 public class Product {
 	
-	private int productId;
-	private int count;
-	private String productName;
-	private double price;
+	private int productId; //product's id
+	private int count; //count created objects
+	private String productName; //product's name
+	private double price; //product's price
 	
-	public Product(int productId, String productName, double price) {
+	/**
+	 * Full constructor
+	 *
+	 * @param productName
+	 *        Product's name.
+	 * @param price
+	 *        Product's price.
+	*/
+	
+	public Product(String productName, double price) {
 		this.productName = productName;
 		this.price = price;
 		setProductId(count++);
+	}
+	
+	/**
+	* Default constructor
+	*
+	*/
+
+	public Product(){
+
 	}
 	
 	public void setProductId(int productId) {
